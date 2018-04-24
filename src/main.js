101e1213f4bcc65e3ec0ebcdc23b09d016302e4d
Isaac Shoman and Matt Dowdney <isaac@>
2018-04-24 15:24:59 -0700

    Add map functionality

--- src/main.js
+++ src/main.js
@@ -4,12 +4,24 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
+const mapsapi = require( 'google-maps-api' )(process.env.GOOGLE_MAP_KEY);
 
-
+function mapFunction () {
+  mapsapi().then( function( maps )  {
+    let map = new maps.Map(document.querySelector('#map'), {
+      center: {
+        lat: 25,
+        lng: 35
+      },
+      zoom: 2
+    });
+  });
+}
 
 
 $(document).ready(function() {
   let flu = new Disease("Flu");
+  mapFunction();
   flu.diseaseSpread();
   flu.spreadRateIncrease();
   setInterval(function(){
@@ -21,26 +33,22 @@ $(document).ready(function() {
     $("#dead").text(`${flu.infectedDead}/100`);
     $("#cure").text(`${flu.cure}/100`);
   }, 1000);
-
-  let promise = new Promise(function(resolve, reject) {
-    let request = new XMLHttpRequest();
-    let url = `http://api.giphy.com/v1/gifs/search?q=disease&limit=1&api_key=${process.env.GIPHY_KEY}`;
-    request.onload = function() {
-      if (this.status === 200) {
-        resolve(request.response);
-      } else {
-        reject(Error(request.statusText));
-      }
-    }
-    request.open("GET", url, true);
-    request.send();
-  });
-
-  promise.then(function(response) {
-    let body = JSON.parse(response);
-    console.log(body);
-    $('#showGif').append(`<img src="${body.data.images.original.url}" alt="sick">`);
-  }, function(error) {
-    $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-  });
 });
+
+//     let coords = [[48.8566,2.3522],[40.7128, -74.0060],[22.3964,114.1095],[35.6895,139.6917],[-33.8688,151.2093],[-33.9249,18.4241],[-15.7942,-47.8822]];
+//     for(let i =0; i<coords.length; i++) {
+//       let marker = new maps.Circle({
+//         strokeColor: '#FF0000',
+//         strokeOpacity: 0.8,
+//         strokeWeight: 2,
+//         fillColor: '#FF0000',
+//         fillOpacity: 0.35,
+//         radius: tickRadius,
+//         map: map,
+//         center: {lat: coords[i][0], lng: coords[i][1]}
+//       });
+//     }
+//   }).catch(function (error) {
+//     console.error(error)
+//   });
+// }
